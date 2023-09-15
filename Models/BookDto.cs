5423c50d294f79f6ab9ba32b3ce363c70b49b797
karani-ken <academice79@gmail.com>
2023-09-15 11:56:42 +0300

    Add project files.

---
+++ Models/BookDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Models
+{
+    public class BookDto
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+       
+        public List<Author> Authors { get; set; } = new List<Author>();
+    }
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
