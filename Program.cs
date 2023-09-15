c6aa785dc9e383b8d021ab57a7424772b984e723
karani-ken <academice79@gmail.com>
2023-09-15 16:35:14 +0300

    Updates and Toast Messages

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using BlazorToastify;
 using Books;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,4 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddToasts();
 await builder.Build().RunAsync();
